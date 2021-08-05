13daad9acf58f32e22ed521f3de536c365018f22
ramirezramiro <88079078+ramirezramiro@users.noreply.github.com>
2021-08-05 15:17:23 +0800

    Delete timestamp.js

--- databasewiring/timestamp.js
+++
@@ -1,4 +0,0 @@
-let currentDate = new Date().toLocaleString([], {timeZone: "Asia/Taipei", hourCycle: 'h23',  seconds: 'long'});
-msg.payload = {object:msg.payload, value:currentDate}; 
-
-return msg; 
